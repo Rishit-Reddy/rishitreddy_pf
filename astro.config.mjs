70cc2971857fe5e36e5b2f3b873c7ccf88c80aab
Rishit Reddy <80318887+Rishit-Reddy@users.noreply.github.com>
2025-06-20 17:03:17 +0530

    fix build issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()]
-	},
-
+    plugins: [tailwindcss()],
+    build: {
+      // Improve error reporting
+      sourcemap: true,
+    },
+    resolve: {
+      preserveSymlinks: true
+    }
+  },
   integrations: [react()]
 });
